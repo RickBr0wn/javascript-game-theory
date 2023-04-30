9d192342739130b09e59ee22efefe131dea2bf9e
Rick Brown <ricky.brown.00@gmail.com>
2023-04-30 09:43:11 +0100

    adds enemy2

---
+++ enemy_movement/script.js
@@ -0,0 +1,80 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+
+const CANVAS_WIDTH = (canvas.width = 500)
+const CANVAS_HEIGHT = (canvas.height = 1000)
+
+const numberOFEnemies = 10
+
+let gameFrame = 0
+
+const enemies = []
+
+class Enemy {
+  constructor(image) {
+    this.image = new Image()
+    this.image.src = image
+
+    this.speed = Math.random() * 4 + 1
+    this.spriteWidth = 218
+    this.spriteHeight = 177
+    this.width = this.spriteWidth / 2.5
+    this.height = this.spriteHeight / 2.5
+    this.x = Math.random() * (CANVAS_WIDTH - this.width)
+    this.y = Math.random() * (CANVAS_HEIGHT - this.height)
+    this.frame = 0
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1)
+    this.angle = 0
+    this.angleSpeed = Math.random() * 0.2
+    this.curve = Math.random() * 7
+  }
+
+  update() {
+    this.x -= Math.sin((this.angle * Math.PI) / 180) * this.speed
+    // if (this.x + this.width < 0) this.x = CANVAS_WIDTH
+    this.y += this.curve * Math.sin(this.angle)
+    this.angle += this.angleSpeed
+    // animate sprites
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++
+    }
+    if (gameFrame > 100) gameFrame = 0
+  }
+
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    )
+  }
+}
+
+for (let i = 0; i < numberOFEnemies; i++) {
+  enemies.push(new Enemy('assets/enemy3.png'))
+}
+
+function clearCanvasBeforeEveryFrame() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+}
+
+function animateLoop() {
+  clearCanvasBeforeEveryFrame()
+
+  enemies.forEach(enemy => {
+    enemy.update()
+    enemy.draw()
+  })
+
+  gameFrame++
+  requestAnimationFrame(animateLoop)
+}
+
+animateLoop()
